2a92fdbd762cd62e551cbe90c6d8224ee104d8af
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-11-27 11:17:26 +0000

    saved the results of fetching in the user variable. Then created a mini constructor called newUser to pull necessary data from the fetching info and add extra key-value propery called money. then created another function to push a newUser to our globally scopped array

--- script.js
+++ script.js
@@ -5,16 +5,31 @@ const showMillionairesBtn = document.getElementById("show-millionaires");
 const sortBtn = document.getElementById("sort");
 const calculateWealthBtn = document.getElementById("calculate-wealth");
 
-let data = [];
+let userList = [];
+
+getRandomUser();
+getRandomUser();
+getRandomUser();
 
 //Fetch random user and add money
 
 async function getRandomUser() {
   const res = await fetch("https://randomuser.me/api");
   const data = await res.json();
+  const user = data.results[0]; //that will come back with an object with a lot of stuff on it so the next step is to create a small code snipet that will take that pulp of info on the user object and format it for us (it will pull only first and last name). Additionally we are going to add another key-value property 'money'
 
-  console.log(data);
+  //here is our actual constructor for the user
+  const newUser = {
+    name: `${user.name.first} ${user.name.last}`,
+    money: Math.floor(Math.random() * 1000000), //that will generate a new number up to a million
+  };
+  addData(newUser);
+  setTimeout(() => {
+    console.log(userList[0]);
+  }, 5000);
 }
-getRandomUser();
-getRandomUser();
-getRandomUser();
+
+//Add new object/user to data array
+function addData(obj) {
+  userList.push(obj);
+}
