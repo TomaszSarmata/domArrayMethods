7fc689278e04c22e907e42de9ccb8b98684fc58c
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-11-27 10:32:14 +0000

    implemented getRandomUser to fetch data from 3rd party api

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ const doubleBtn = document.getElementById("double");
 const showMillionairesBtn = document.getElementById("show-millionaires");
 const sortBtn = document.getElementById("sort");
 const calculateWealthBtn = document.getElementById("calculate-wealth");
+
+let data = [];
+
+//Fetch random user and add money
+
+async function getRandomUser() {
+  const res = await fetch("https://randomuser.me/api");
+  const data = await res.json();
+
+  console.log(data);
+}
+getRandomUser();
